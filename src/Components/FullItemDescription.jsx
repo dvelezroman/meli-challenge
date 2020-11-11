a8ba59ecf8ea3f7d87a12cd221512257a9d036db
Dario Velez Roman <dvelezroman@gmail.com>
2020-11-11 08:15:21 -0300

    Full Item description mock ready

--- src/Components/FullItemDescription.jsx
+++ src/Components/FullItemDescription.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect } from 'react'
 import { useQuery } from '../utils'
 
+import '../styles/FullItem.scss'
+import BreadCrumb from './Breadcrumb'
+
+import response from '../data/query.json'
+
 const FullItemDescription = () => {
   const queryParams = useQuery()
 
@@ -9,8 +14,19 @@ const FullItemDescription = () => {
     console.log({ id })
   }, [queryParams])
 
+  const { categories } = response.data.search
+
   return (
-    <div>Full Item Description</div>
+    <div className="Full-Item-Container">
+      <BreadCrumb categories={categories} />
+      <div className="Description">
+        <div className="Top">
+          <div className="Full-Image"></div>
+          <div className="Full-Data"></div>
+        </div>
+        <div className="Bottom"></div>
+      </div>
+    </div>
   )
 }
 
