89c937e3287274d3fd7483dafdb6cf42f16d151f
Dario Velez Roman <dvelezroman@gmail.com>
2020-11-11 19:57:56 -0300

    full description item design ready

--- src/Components/FullItemDescription.jsx
+++ src/Components/FullItemDescription.jsx
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react'
-import { useQuery } from '../utils'
+import React, { useEffect, useMemo } from 'react'
+import { useQuery, formatter } from '../utils'
 
 import '../styles/FullItem.scss'
 import BreadCrumb from './Breadcrumb'
 
 import response from '../data/query.json'
 import itemResponse from '../data/item.json'
+import ImageComponent from './ImageComponent'
 
 const FullItemDescription = () => {
   const queryParams = useQuery()
@@ -18,18 +19,36 @@ const FullItemDescription = () => {
   const { categories } = response.data.search
   const { item } = itemResponse.data.getItem
 
+  const priceFormatted = useMemo(() => {
+    const priceArray = formatter(item.price.currency, item.price.decimals).format(item.price.amount).split(',')
+    return {
+      amount: priceArray[0],
+      decimals: priceArray[1],
+    }
+  }, [item])
+
   return (
     <div className="Full-Item-Container">
       <BreadCrumb categories={categories} />
       <div className="Description">
         <div className="Top">
-          <div className="Full-Image"></div>
-          <div className="Full-Data"></div>
+          <div className="Full-Image">
+            <ImageComponent src={item.picture} className="Product-Image" alt='Product Image' />
+          </div>
+          <div className="Full-Data">
+            <span className="Item-Condition">{item.condition === 'new' ? 'Nuevo' : 'Usado'} - {item.soldQuantity} vendidos</span>
+            <span className="Item-Name">{item.title}</span>
+            <span className="Item-Price">
+              <span>{priceFormatted.amount}</span>
+              <span className="Decimals">{priceFormatted.decimals}</span>
+            </span>
+            <button className="Item-Buy-Button">Comprar</button>
+          </div>
         </div>
         <div className="Bottom">
           <div className="Bottom-Left">
-            <h4>Descripción del producto</h4>
-            <p className="Full-Item-Description">{item.description}</p>
+            <h3>Descripción del producto</h3>
+            <span className="Full-Item-Description">{item.description}</span>
           </div>
           <div className="Bottom-Right"></div>
         </div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -5,3 +5,11 @@ import {
 export const useQuery = () => {
   return new URLSearchParams(useLocation().search);
 }
+
+export const formatter = (currency, decimals) => {
+    return new Intl.NumberFormat('es-AR', {
+    style: 'currency',
+    currency,
+    minimumFractionDigits: decimals,
+  })
+}
