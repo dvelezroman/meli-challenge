d6e654b6e6617fe3b65459bbc5d3752afa165331
Dario Velez Roman <dvelezroman@gmail.com>
2020-11-12 15:13:20 -0300

    minor fixes

--- src/Components/SearchBox.jsx
+++ src/Components/SearchBox.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useRef } from 'react'
+import React, { useCallback, useEffect, useRef } from 'react'
 import "../styles/SearchBox.scss"
 import ImageComponent from './ImageComponent'
 
